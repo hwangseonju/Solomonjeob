40ba080a500e2eb4d5485f06055dc3b126b813de
juyong <ht9943@naver.com>
2022-01-19 17:30:45 +0900

    Feat: create signup page
    기본 환경설정, Vue 프로젝트 생성, 회원가입 탬플릿 생성

---
+++ FE/frontend/src/main.js
@@ -0,0 +1,18 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from '@/routes/index'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+// 	render: h => h(App),
+// 	router,
+// }).$mount('#app')
+
+import { createApp } from 'vue'
+import App from '@/App.vue'
+import router from '@/router'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
