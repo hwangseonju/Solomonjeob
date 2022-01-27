a32542be4b0359ca4d6c2ca033b686a4987eaa41
노건우 <ironylovely@naver.com>
2022-01-28 01:51:42 +0900

    Feat: modify questionanswerlist
    질문모음집 리스트 코드 수정

--- FE/frontend/src/App.vue
+++ FE/frontend/src/App.vue
@@ -10,14 +10,28 @@
 <script>
 import AppHeader from '@/components/common/AppHeader.vue';
 import Footer from '@/components/Footer.vue'
+// import QuestionPage from '@/views/QuestionPage.vue'
+// import LoginPage from '@/vies/Member/LoginPage.vue'
 // import Home from '@/views/Home.vue';
 
 export default {
   components: {
     AppHeader,
-    Footer
-    
+    Footer,
+    // QuestionPage,
+    // LoginPage
   },
+  data() {
+    return {
+      // memberIdx: 0,
+    }
+  },
+  // methods: {
+  //   getMemberIdx(Idx) {
+  //     this.memberIdx = Idx
+  //   }
+
+  // }
 };
 </script>
 
--- FE/frontend/src/components/QuestionAnswerForm.vue
+++ FE/frontend/src/components/QuestionAnswerForm.vue
@@ -6,24 +6,17 @@
         <div class="position-sticky pt-3">
           <ul class="nav flex-column">
             <button @click="insertQuestionList">+</button>
-            <li v-for="index in this.questionlist" :key="index" class="nav-item">
+            <li v-for="question in this.questionList" :key="question" class="nav-item" >
               <div>          
-                <input class="quesinput" type="text" :disabled="disabled == 1" value="{{ index }}">
-                <button @click="disabled = (disabled + 1) % 2">o</button>
+                <input class="quesinput" type="text" :disabled="disabled != question.qnasId" v-model="question.qnasTitle" >
+                
+                <button type="button" @click="editQuestion(question.qnasId)">o</button>
                 <button>x</button>
               </div> 
               
+               
             </li>
           </ul>
-
-          <!-- <ul class="nav flex-column ">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file-text"></span>
-                Current month
-              </a>
-            </li>
-          </ul> -->
         </div>
       </nav>
 
@@ -44,7 +37,7 @@
 
         <!-- <canvas class="my-4 w-100" id="myChart" width="900" height="380"> -->
           <ul>
-            <li v-for="question in this.quesanswerlist" :key="question" >
+            <li v-for="question in this.questionAnswerList" :key="question" >
               <!-- {{ question[0] }} -->
               <textarea placeholder="질문을 입력해주세요" name="" id="" cols="130" rows="10">
               </textarea>
@@ -61,24 +54,105 @@
 </template>
 
 <script >
-             
+import { instance } from '@/api/index.js'             
 export default {
+
   
   data () {
     return{
-
- 
-      disabled: 1
+      insertQuestion:{
+        qnasMemberId : 1,
+        qnasTitle: '질문모음집',
+      },
+      question: {
+        qnasTitle: ''
+        },
+      memberIdx: 0,
+      disabled: 0,
+      questionList:[],
+      questiontitle: '',
+      questionAnswerList:[],
     }
   },
   methods : {
-    editQuestion(index) {
-      this.edit.text = index
+    getToken: function () {
+      const token = localStorage.getItem('jwt')
+      const config = {
+        'jwt-auth-token': token
+      }
+      return config
+    },
+    getMemberIdx() {
+      this.memberIdx = localStorage.getItem('memberIdx')
+      
+    },
+     getQuestionList() {
+      const memberIdx = this.getMemberIdx
+      instance({
+        method: 'get',
+        url: '/api/qnas/my/' + this.memberIdx,
+        data: {qnasMemberId:memberIdx , qnasTitle:'질문모음집'},
+        headers: this.getToken()
+      })
+      .then(res => {
+        console.log(res)
+        this.questionList = res.data
+        
+      })
     },
     insertQuestionList() {
+      instance({
+        method: 'post',
+        url: '/api/qnas',
+        data: {qnasMemberId:this.memberIdx , qnasTitle:'질문모음집'},
+        headers: this.getToken()
+      })
+      .then(res => {
+        console.log(res)
+        this.$router.go()
+      })
+      .catch(err => {
+        console.log(err)
+        alert('실패')
+      })
+    },
+    // editQues(event) {
+    //   var updatedText = event.target.value
+    //   this.qnasTitle = updatedText
+    // },
+    editQuestion(number) {
+      if (this.disabled != number){
+        this.disabled = number}
+        
+      else {
+        instance({
+        method: 'put',
+        url: '/api/qnas/' + number  ,
+        data: {qnasCode:number,
+          qnasTitle: this.question.qnasTitle,
+          qnasMemberId:this.memberIdx},
+        headers: this.getToken()
+      })
+      .then(res => {
+        console.log(res)
+        this.disabled = 0
+        // this.$router.go()
+      })
+      .catch(err => {
+        console.log(err)
+        alert('실패')
+      })        
+        
+      }
 
     }
+ 
 
+  },
+  created() {
+    this.getMemberIdx()
+    this.getQuestionList()
+    
   }
 }
 </script>
